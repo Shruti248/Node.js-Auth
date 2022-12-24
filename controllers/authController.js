da510fbc618a11d1f73336daf42f6b6cf186ab4e
shruti248 <shrutiunadkat5@gmail.com>
2022-12-24 10:58:35 +0530

    Mongoose Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,36 @@
 const User = require('../models/User');
 
+// Handle Erros
+const handleErros = (err) => {
+    // err.code will be useful for the unique property of the email , else the value is undefined
+    console.log(err.message , err.code);
+
+    let errors = {email : '' , password : ''};
+
+    // duplicate error.code
+    if(err.code == 11000){
+        errors.email = 'Email is already registered';
+        return errors;
+    }
+
+    // validation errors
+    if(err.message.includes('user validation failed')){
+        // console.log(err);
+        // Observer the err in console first 
+        // we want the err overall object , then the errors object inside the err and then we want values of the properties(Object.values)
+        // We ultimately require the properties as that is what we want to print
+        // console.log(Object.values(err.errors));
+
+        // the result is array of value , therefore...
+        Object.values(err.errors).forEach(({properties}) => {
+            // console.log(properties);
+            errors[properties.path] = properties.message;
+        })
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req , res) => {
     res.render('signup');
 }
@@ -15,8 +46,8 @@ module.exports.signup_post = async (req , res) => {
         })
         res.status(201).json({user});
     }catch(err){
-        console.log(err);
-        res.status(400).send('error , user not created');
+        const errors =  handleErros(err);
+        res.status(400).json({errors});
     }
 
 }
--- models/User.js
+++ models/User.js
@@ -1,19 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const {isEmail} = require('validator');
 
 const userSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        required : true,
-        unique:true,
-        lowercase:true,
-    },
-    password:{
-        type : String,
-        required : true,
-        minLength:6,
-    },
+  email: {
+    type: String,
+    // Custom error Messages for the different conditions
+    // required : [if true , else print error message]
+    required: [true, "Please enter an email"],
+    // unique: [true , "Email is already taken"], 
+    // cannot do this customer validation like other fields , therefire handle error code
+    unique:true,
+    lowercase: true,
+    // custom validation using regExp or third party validation package(using validator package)
+    validate: [isEmail , "Please enter a valid Email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minLength: [6, "Minimum password length is 6 characters"],
+  },
 });
 
-const User = mongoose.model('user' , userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
