c4372a44384d170230c2eb1b451e75483880545b
shruti248 <shrutiunadkat5@gmail.com>
2022-12-24 11:26:10 +0530

    Hashing Passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -25,21 +26,31 @@ const userSchema = new mongoose.Schema({
 // Eg : a mongoose hoook that is fired after the doc is saved , deleted to db 
 
 // fire a function after(post) the doc is saved to db
-userSchema.post('save' , function(doc , next){
-    console.log('New User was created and saved' , doc);
-    next();
-})
+// userSchema.post('save' , function(doc , next){
+//     console.log('New User was created and saved' , doc);
+//     next();
+// })
 
 // fire a function before(pre) the doc is saved to db
-userSchema.pre('save' , function(next) {
-    // this : will create local instance before it is saved to db 
-    console.log('User about to be created & saved' , this);
-    next();
-})
+// userSchema.pre('save' , function(next) {
+//     // this : will create local instance before it is saved to db 
+//     console.log('User about to be created & saved' , this);
+//     next();
+// })
 
 // The pre hook is used to hash the passwords before it is saved to the db 
 // Refer : https://dev.to/sahilthakur7/pre-and-post-hooks-in-mongoose-16be
 
+// Hashing Using Bcrypt
+// 2 ways 
+// 1) Password --- Hashing Algorithm --- Hashed Algo
+// 2) Salt + Password --- Hashing Algorithm -- Hashed algo considering the whoe string salt and the original password (More Safe)
+
+userSchema.pre('save' , async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password  , salt);
+    next();
+})
 
 const User = mongoose.model("user", userSchema);
 
